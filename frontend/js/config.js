b48be39b75ee0e301f4e6d7c934fe96389b93a5d
AustinCLieu <asianonrice@gmail.com>
2026-05-14 01:17:04 -0700

    chore(frontend): add BACKEND_URL to config

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,3 +1,3 @@
 export const SUPABASE_URL = 'https://rwycvntrglluxcmhnnqy.supabase.co';
 export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ3eWN2bnRyZ2xsdXhjbWhubnF5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg1NzAwOTQsImV4cCI6MjA5NDE0NjA5NH0.-JeU580uYlkHfiFRPoeG0Rz__Vr60WpBNmoQWfxTUIQ';
-export const API_BASE = 'http://localhost:3000';
+export const BACKEND_URL = 'http://localhost:3000';
