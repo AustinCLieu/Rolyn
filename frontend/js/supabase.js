a269022c9182451cea1226bdbc8ce5228e5569e3
AustinCLieu <asianonrice@gmail.com>
2026-05-14 01:18:14 -0700

    fix(frontend): correct SUPABASE_ANON_KEY typo

--- frontend/js/supabase.js
+++ frontend/js/supabase.js
@@ -1,4 +1,4 @@
 import { createClient } from 'https://cdn.skypack.dev/@supabase/supabase-js';
 import { SUPABASE_URL, SUPABASE_ANON_KEY } from './config.js';
 
-export const supabase = createClient(SUPABASE_URL, SUIPABASE_ANON_KEY);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
